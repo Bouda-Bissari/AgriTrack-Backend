4a559d3c875208bf3408747a2834518b17560a77
Cyrille-18 <cyrillepio@hotmail.com>
2025-04-08 15:46:36 +0000

    Feat(Models): Create intervention model
    create model for interventions with all require field and relations

---
+++ app/Models/Intervention.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Intervention extends Model
+{
+    protected $fillable = [
+        'title',
+        'type',
+        'isDone',
+        'interventionDate',
+        'productQuantity',
+        'description',
+        'land_id'
+    ];
+    /**
+     * Terrain sur lequel cette intervention est réalisée.
+     */
+    public function land(): BelongsTo
+    {
+        return $this->belongsTo(Land::class);
+    }
+    /**
+     * Candidatures des travailleurs intéressés par cette intervention.
+     */
+    public function candidatures(): HasMany
+    {
+        return $this->hasMany(Candidature::class);
+    }
+    /**
+     * Notifications liées à cette intervention.
+     */
+    public function notifications(): HasMany
+    {
+        return $this->hasMany(Notification::class);
+    }
+}
