ae6d5dd3e7893a2d074659c3a482769621e47ea7
Cyrille-18 <cyrillepio@hotmail.com>
2025-04-08 15:43:38 +0000

    Feat(Models): Create Land Model
    create land models with all require field

---
+++ app/Models/Land.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Land extends Model
+{
+    protected $fillable = [
+        'name',
+        'date',
+        'city',
+        'cultureType',
+        'area',
+        'ownershipdoc',
+        'latitude',
+        'longitude',
+        'statut',
+        'user_id'
+    ];
+
+    /**
+     * Propriétaire du terrain.
+     */
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    /**
+     * Liste des interventions demandées sur ce terrain.
+     */
+    public function interventions(): HasMany
+    {
+        return $this->hasMany(Intervention::class);
+    }
+}
