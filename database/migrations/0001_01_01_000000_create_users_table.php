fae4c755a86386bbd1e22274f1523e8864872dcd
Cyrille-18 <cyrillepio@hotmail.com>
2025-04-08 16:01:57 +0000

    Feat(Migrations): Update Create_user_table
    add new colums and remove timestamp()

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,18 +13,20 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('bio')->nullable();
             $table->string('email')->unique();
+            $table->string('phoneNumber');
+            $table->enum('role', ['landOwner', 'worker', 'admin']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
-            $table->timestamp('created_at')->nullable();
         });
 
         Schema::create('sessions', function (Blueprint $table) {
